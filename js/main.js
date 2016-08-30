75eddd166deb4ae208bbd8c8b28d0c2be67df5e3
diljitvj@gmail.com <diljitvj@gmail.com>
2016-08-30 16:12:50 +0530

    Template test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+var users = kendo.observable({
+    users: []
+});
+$("#getUsers").click(getUsers);
+$("#resetUsers").click(reset);
 
+function getUsers() {
+    let userNum = $("input").val();
+    //alert(userNum);
+    //users = undefined;
+    $.ajax({
+        url: 'https://randomuser.me/api/?results=' + userNum,
+        dataType: 'json',
+        success: function (data) {
+            let temp = users.get("users");
+            console.log(temp);
+            for (let i = 0; i < userNum; i++) {
+                const jsonData = data.results[i];
+                temp.push({
+                    firstname: jsonData.name.first,
+                    lastname: jsonData.name.last,
+                    email: jsonData.email,
+                    picUrl: jsonData.picture.large
+                });
+
+            }
+
+            console.log(temp);
+            var template = kendo.template($("#javascriptTemplate").html());
+            var result = template(users);
+            $("#userList").html(result);
+        }
+    });
+
+}
+
+function reset() {
+    var users = kendo.observable({
+        users: []
+
+    });
+    var template = kendo.template($("#javascriptTemplate").html());
+    var result = template(users);
+    $("#userList").html(result);
+    console.log(users);
+}
